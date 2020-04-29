cd7afb560646c1e9482d356addb86bdaa42004df
Saharsh Bhatia <friedchickenfordinner@gmail.com>
2020-04-29 12:40:35 +1000

    Add description field for type

--- locations/models.py
+++ locations/models.py
@@ -3,6 +3,7 @@ from django.contrib.gis.db import models
 class Type(models.Model):
     # Type Model
     name = models.CharField(max_length=250)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
