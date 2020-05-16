32514cc6839febfd796f388a4d8342cbdb694d47
Saharsh Bhatia <friedchickenfordinner@gmail.com>
2020-05-16 17:25:31 +1000

    Changes to page serializer

--- page_category/serializers.py
+++ page_category/serializers.py
@@ -2,9 +2,8 @@ from rest_framework import serializers
 
 from .models import Page
 
-class PageSerializer(serializers.HyperlinkedModelSerializer):
+class PageSerializer(serializers.ModelSerializer):
     # Type serializer
     class Meta:
         model = Page
-        fields = ['id','name','banner_image','description','categories']
         fields = ['id','name','slug','banner_image','description','categories']
