7324a78dc5124b75bc3d6dd880d947427e30be08
Saharsh Bhatia <friedchickenfordinner@gmail.com>
2020-05-18 03:14:36 +1000

    Frontend build

--- locations/admin.py
+++ locations/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 from .models import Location, Type
 
 # Register admin dashboard for Type & Location
+class LocationAdmin(admin.ModelAdmin):
+    model = Location
+    search_fields = ['name']
+
 admin.site.register(Type)
-admin.site.register(Location)
+admin.site.register(Location,LocationAdmin)
